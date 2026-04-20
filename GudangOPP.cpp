b0b349eea9e6c578ac5940dc507746854f955db0
“BANTENGSELAM” <pedjuangjava@gmail.com>
2026-04-20 14:59:22 +0700

    GudangOPP

---
+++ GudangOPP.cpp
@@ -0,0 +1,40 @@
+#include <iostream>; // library
+using namespace std;
+
+class Barang{
+    public :
+
+    //penambahan int
+    string  nama;
+    string  kategori;
+    int     jumlah;
+    string  tanggalProduksi;
+
+    void printData(){ //void output
+            cout << "Nama Barang        : " << nama << endl;
+            cout << "Kategori           : " << kategori << endl;
+            cout << "Jumlah             : " << jumlah << endl;
+            cout << "Tanggal Produksi   : " << tanggalProduksi << endl;
+    }
+
+    
+};// batas class
+
+//program utama
+int main (){
+    Barang brEl;
+    brEl.nama                 = "TV";
+    brEl.kategori             = "Elektronik";
+    brEl.jumlah               = 2;
+    brEl.tanggalProduksi      = "2026-04-20";
+    brEl.printData();
+
+    cout << endl;
+
+    Barang brNL;
+    brNL.nama                 = "Meja";
+    brNL.kategori             = "Non-Elektronik";
+    brNL.jumlah               = 5;
+    brNL.tanggalProduksi      = "2026-04-25";
+    brNL.printData();
+}
--- IntroductionOPPpart2.cpp
+++
@@ -1,2 +0,0 @@
-#include <iostream>; // library
-using namespace std;
