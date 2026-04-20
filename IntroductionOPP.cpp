b01dddd23500de5d063aff97bc86f5fbdad9c29e
“BANTENGSELAM” <pedjuangjava@gmail.com>
2026-04-20 14:23:29 +0700

    void output

--- IntroductionOPP.cpp
+++ IntroductionOPP.cpp
@@ -9,4 +9,9 @@ class Mahasiswa{ // Class Mahasiswa
     string nama;
     float nilai;
     
-}
+    void printData(){ //void output
+            cout << "NIM    : " << nim << endl;
+            cout << "Nama    : " << nama << endl;
+            cout << "Nilai   : " << niilai << endl;
+    }
+};// batas class
